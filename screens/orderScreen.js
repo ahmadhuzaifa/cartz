cc41a89a624d1c2c3ed66a356f4b1511e0d56642
Huzaifa Ahmad <huzaifaahmad@Huzaifas-MacBook-Pro.local>
2020-04-27 16:05:45 -0700

    add orders

--- screens/orderScreen.js
+++ screens/orderScreen.js
@@ -25,35 +25,41 @@ class OrderScreen extends React.Component {
         return (
             <Container>
                 <ScrollView>
-                <StatusBar hidden />
-                <Cover>
-                    <Image source={order.image} />
-                    <Title>{order.cart_title}</Title>
-                    <Caption>{order.order_title}</Caption>
-                </Cover>
-                <TouchableOpacity
-                onPress={() => {
-                    this.props.navigation.goBack();
-                }}
-                style={{
-                    position: "absolute",
-                    top: 20,
-                    right: 20
-                }}
-                >
-                <CloseView>
-                    <Ionicons 
-                        name="ios-close"
-                        size={36}
-                        style={{ marginTop: -2 }}
-                        color="#4775f2"/>
-                </CloseView>
-                </TouchableOpacity>
-                <Wrapper>
-                    <Logo source={order.deliverer_image} />
-                    <Subtitle>{order.deliverer}</Subtitle>
-                </Wrapper>
-
+                    <StatusBar hidden />
+                    <Cover>
+                        <Image source={order.image} />
+                        <Title>{order.cart_title}</Title>
+                        <Caption>{order.order_title}</Caption>
+                    </Cover>
+                    <TouchableOpacity
+                    onPress={() => {
+                        this.props.navigation.goBack();
+                    }}
+                    style={{
+                        position: "absolute",
+                        top: 20,
+                        right: 20
+                    }}>
+                        <CloseView>
+                            <Ionicons 
+                                name="ios-close"
+                                size={36}
+                                style={{ marginTop: -2 }}
+                                color="#4775f2"/>
+                        </CloseView>
+                    </TouchableOpacity>
+                    <Wrapper>
+                        <Logo source={order.deliverer_image} />
+                        <Subtitle>{order.deliverer}</Subtitle>
+                    </Wrapper>
+                    <Content>
+                        <OrderContainer>
+                            <OrderTitle>Edison Li</OrderTitle>
+                            <OrderButtonContainer>
+                                <OrderButtonTitle>Accept</OrderButtonTitle>
+                            </OrderButtonContainer>
+                        </OrderContainer>
+                    </Content>
                 </ScrollView>
 
             </Container>
@@ -65,67 +71,105 @@ export default OrderScreen
 
 
 const Container = styled.View`
-flex: 1;
+    flex: 1;
 `;
 
 const Cover = styled.View`
-height: 305px;
+    height: 305px;
 `;
 
 const Image = styled.Image`
-height: 100%;
-width: 100%;
-position: absolute;
-top: 0;
-background: #3c4560;
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    background: #3c4560;
 `;
 
 const Title = styled.Text`
-font-size: 24px;
-color: white;
-font-weight: bold;
-width: 170px;
-position: absolute;
-top: 78px;
-left: 20px;
+    font-size: 24px;
+    color: white;
+    font-weight: bold;
+    width: 170px;
+    position: absolute;
+    top: 78px;
+    left: 20px;
 `;
 
 const Caption = styled.Text`
-color: white;
-font-size: 17;
-position: absolute;
-bottom: 20px;
-left: 20px;
-max-width: 300px;
+    color: white;
+    font-size: 17;
+    position: absolute;
+    bottom: 20px;
+    left: 20px;
+    max-width: 300px;
 `;
 
 const CloseView = styled.View`
-width: 32px;
-height: 32px;
-background: white;
-border-radius: 22px;
-box-shadow: 0 5px 10px rgba(0, 0, 0, 0.15);
-justify-content: center;
-align-items: center;
+    width: 32px;
+    height: 32px;
+    background: white;
+    border-radius: 22px;
+    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.15);
+    justify-content: center;
+    align-items: center;
 `;
 
 const Wrapper = styled.View`
-flex-direction: row;
-position: absolute;
-top: 40px;
-left: 20px;
-align-items: center;
+    flex-direction: row;
+    position: absolute;
+    top: 40px;
+    left: 20px;
+    align-items: center;
 `;
 
 const Logo = styled.Image`
-width: 24px;
-height: 24px;
+    width: 24px;
+    height: 24px;
 `;
 
 const Subtitle = styled.Text`
-font-size: 15px;
-font-weight: 600;
-color: rgba(255, 255, 255, 0.8);
-margin-left: 5px;
-text-transform: uppercase;
+    font-size: 15px;
+    font-weight: 600;
+    color: rgba(255, 255, 255, 0.8);
+    margin-left: 5px;
+    text-transform: uppercase;
+`;
+const Content = styled.View `
+    height: 100%;
+    background: white;
+    padding: 32px;
+`;
+const OrderContainer = styled.View `
+    background: white;
+    borderRadius: 15px;
+    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.15);
+`;
+
+
+const OrderTitle = styled.Text `
+    color: black;
+    font-weight: 600;
+    font-size: 20px;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    padding-right: 20px;
+    padding-left: 20px;
+`;
+
+const OrderButtonContainer = styled.View `
+    background: #503D9E;
+    borderRadius: 15px;
+    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.15);
+    padding-right: 32px;
+    padding-left: 32px;
+    justify-content: center;
+    align-items: center;
+`
+const OrderButtonTitle = styled.Text `
+    color: #FCD460;
+    font-weight: 600;
+    font-size: 17px;
+    padding-top: 15px;
+    padding-bottom: 15px;
 `;
